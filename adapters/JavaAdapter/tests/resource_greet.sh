1441440ae374b339bff0d78a949079090fa22021
Shinoj Zacharias <shinoj@Shinojs-MacBook-Pro.local>
2018-04-23 16:42:34 +0530

    Add test scripts

--- adapters/JavaAdapter/tests/resource_greet.sh
+++ adapters/JavaAdapter/tests/resource_greet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-mfpdev --version
-echo "#### Test Adapter Endpoint : resource/gree"
+echo "#### Test Adapter Endpoint : resource/greet"
 output=$(mfpdev adapter call javaAdapter/resource/greet --id "test" --secret "test" --query "name:MFP Adapter")
 if [ $? -eq 0 ]; then
 	echo OK
 else
 	echo FAIL
+	exit 1
 fi
 echo "Adapter Call Response : "
 echo $output
--- adapters/JavaAdapter/tests/testbuckets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "#### Calling All the Adapter Tests"
-./resource.sh
-./resource_greet.sh
-./resource_protected.sh
-./resource_testpost.sh
